f67cc10945d0cc3c41d8e5dd2bdb48f92b9a12fb
Umriya Afini <afini.umriya@gmail.com>
2022-03-24 10:08:07 +0700

    Add layout to user page

--- pages/user.js
+++ pages/user.js
@@ -1,19 +1,31 @@
+import Layout from "../components/layout";
+import Head from "next/head";
+import Link from "next/link";
+
 function User({ users }) {
-  console.log(users);
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.login.uuid}>{user.name.first}</li>
-      ))}
-      hei
-    </ul>
+    <Layout>
+      <Head>
+        <title>Users</title>
+      </Head>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.login.uuid}>{user.name.first}</li>
+        ))}
+      </ul>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
   );
 }
 
 export async function getStaticProps() {
   const res = await fetch("https://randomuser.me/api/?results=30");
   const users = await res.json();
-  console.log(users);
 
   return {
     props: {
